41f74851551449d9546ae5801e07999abfa63f8e
ricardoprg2022-commits <ricardoprg2022@gmail.com>
2026-01-24 12:35:52 -0300

    install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@ Write-Host "Baixando arquivo.bat..."
 
 $bat = "$env:TEMP\arquivo.bat"
 
-irm https://raw.githubusercontent.com/SEU_USUARIO/meu-script/main/arquivo.bat -OutFile $bat
+irm https://https://raw.githubusercontent.com/ricardoprg2022-commits/rn_informatica/refs/heads/main/office_key.bat -OutFile $bat
 
 cmd /c $bat
 
